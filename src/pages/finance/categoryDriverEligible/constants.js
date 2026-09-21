daae7bef03cd4f93e06cb4c16467233883696135
jagatheshtexve <jagatheshgtexve@gmail.com>
2026-09-21 19:08:31 +0530

    Update label for 'FREE' driver rule plan to 'FREE PLAN'

--- src/pages/finance/categoryDriverEligible/constants.js
+++ src/pages/finance/categoryDriverEligible/constants.js
@@ -102,7 +102,7 @@ export const DRIVER_RULE_PLAN_OPTIONS = [
   { value: "REGULAR", label: "Regular" },
   { value: "STANDARD", label: "Standard" },
   { value: "PREMIUM", label: "Premium" },
-  { value: "FREE", label: "Free" },
+  { value: "FREE PLAN", label: "Free" },
   { value: "ALL", label: "All" },
 ];
 
